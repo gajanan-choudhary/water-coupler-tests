0761c9c7f1d68bb27ea35183969b48f7e4877836
Gajanan Choudhary <gajanan@utexas.edu>
2021-02-03 19:33:30 -0600

    Add AdH-GSSHA coupled model: Galveston and Brays
    Heavy use of symlinks to make it convenient to run tests without
    repeatedly copying the same files around. Run tests in one of the `adg`,
    `gda`, `adgda`, or `gdadg` folders.

---
+++ coupling-tests/adh-gssha/galriver-brays-discharge/clean.sh
@@ -0,0 +1,16 @@
+rm  brays_harvey.cdp \
+    brays_harvey.cdq \
+    brays_harvey.dep \
+    brays_harvey.drn \
+    brays_harvey.ohl \
+    brays_harvey.sum \
+    brays_harvey.otl \
+    bad_thalweg.out \
+    short_links.out \
+    maskmap \
+    galriver*.h5_back \
+    galriver*.xmf_back \
+    galriver*.h5 \
+    galriver*.xmf \
+
+
