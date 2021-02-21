8bcdef83b05c8c9d01d23780e30a04740ddfb7f7
Gajanan Choudhary <gajanan@utexas.edu>
2021-02-20 22:05:52 -0600

    Add AdH-GSSHA coupled model: Large and Stream

---
+++ coupling-tests/adh-gssha/large-stream-no-overbank/clean.sh
@@ -0,0 +1,15 @@
+rm  Stream.cdp \
+    Stream.cdq \
+    Stream.dep \
+    Stream.drn \
+    Stream.ohl \
+    Stream.sum \
+    Stream.otl \
+    bad_thalweg.out \
+    short_links.out \
+    maskmap \
+    large*.h5_back \
+    large*.xmf_back \
+    large*.h5 \
+    large*.xmf \
+    large*.dat
